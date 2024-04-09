239e14da42238d8085f0d1a372bb4daf781b6517
sokunnea.born <bornsokunnea@gmail.com>
2024-04-09 15:42:45 +0700

    cmt

---
+++ encryptDecrypt/src/main/java/com/example/encryptDecrypt/AESEncryptionDecryption.java
@@ -0,0 +1,62 @@
+package com.example.encryptDecrypt;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class AESEncryptionDecryption {
+    private static SecretKeySpec secretKey;
+    private static byte[] key;
+    private static final String ALGORITHM = "AES";
+    private static final String secretKey1 = "1234";
+    public void prepareSecreteKey(String myKey) {
+        MessageDigest sha = null;
+        try {
+            key = myKey.getBytes(StandardCharsets.UTF_8);
+            sha = MessageDigest.getInstance("SHA-1");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+            secretKey = new SecretKeySpec(key, ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String encrypt(String strToEncrypt, String secret) {
+        try {
+            prepareSecreteKey(secret);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        } catch (Exception e) {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public String decrypt(String strToDecrypt, String secret) {
+        try {
+            prepareSecreteKey(secret);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String originalString = "1234567890";
+        AESEncryptionDecryption aesEncryptionDecryption = new AESEncryptionDecryption();
+        String encryptedString = aesEncryptionDecryption.encrypt(originalString, secretKey1);
+        String decryptedString = aesEncryptionDecryption.decrypt(encryptedString, secretKey1);
+        System.out.println(originalString);
+        System.out.println(encryptedString);
+        System.out.println(decryptedString);
+    }
+}
---
+++ encryptDecrypt/src/test/java/com/example/encryptDecrypt/EncryptDecryptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.encryptDecrypt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptDecryptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
